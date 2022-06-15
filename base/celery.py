6158c5c3104cd7e418ebbc6ea20f904f20c01e0a
edd19 <eddyndizera@gmail.com>
2022-06-15 13:12:07 +0200

    fix read env for celery.py

--- base/celery.py
+++ base/celery.py
@@ -29,13 +29,13 @@ import os
 import sys
 
 from celery import Celery
-from dotenv import load_dotenv
+import dotenv
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if 'test' in sys.argv:
     os.environ.setdefault('TESTING', 'True')
 
-load_dotenv(os.path.join(BASE_DIR, '.env'))
+dotenv.read_dotenv(os.path.join(BASE_DIR, '.env'))
 sys.path.extend(os.environ.get('EXTRA_SYS_PATHS').split()) if os.environ.get('EXTRA_SYS_PATHS') else None
 
 SETTINGS_FILE = os.environ.get('DJANGO_SETTINGS_MODULE', 'base.settings')
