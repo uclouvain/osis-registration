ac01c2ca91ace28f03e0a9e8d4d2c89845299dc8
edd19 <eddyndizera@gmail.com>
2022-06-15 13:42:58 +0200

    always generate allowed hosts from env

--- base/settings.py
+++ base/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = [] if DEBUG else os.environ.get('ALLOWED_HOSTS', "['*']")
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', "[]")
 
 
 # Application definition
